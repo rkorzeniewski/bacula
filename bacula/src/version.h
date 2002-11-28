bb0d1b86be2638f5eed37dd61daa0bcb42f0d278
Kern Sibbald <kern@sibbald.com>
2002-11-28 10:46:12 +0000

    Bump version
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@220 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -1,8 +1,8 @@
 /* */
 #define VERSION "1.27"
 #define VSTRING "1"
-#define DATE    "26 November 2002"
-#define LSMDATE "26Nov02"
+#define DATE    "28 November 2002"
+#define LSMDATE "28Nov02"
 
 /* Debug flags */
 #define DEBUG 1
