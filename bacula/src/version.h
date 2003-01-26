561133a0fc5b86ced75f04dd3b1051390fddec2e
Kern Sibbald <kern@sibbald.com>
2003-01-26 15:15:32 +0000

    Misc config changes
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@324 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -1,8 +1,8 @@
 /* */
-#define VERSION "1.29"
+#define VERSION "1.30"
 #define VSTRING "1"
-#define DATE    "22 January 2003"
-#define LSMDATE "22Jan03"
+#define DATE    "26 January 2003"
+#define LSMDATE "26Jan03"
 
 /* Debug flags */
 #define DEBUG 1
