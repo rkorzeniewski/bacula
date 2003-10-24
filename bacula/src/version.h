cf7f0996fb36c824395cec56abd6b375d329beea
Kern Sibbald <kern@sibbald.com>
2003-10-24 16:49:01 +0000

    Fix non-portable var.c + document changes
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@770 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -2,8 +2,8 @@
 #undef  VERSION
 #define VERSION "1.32c"
 #define VSTRING "1"
-#define BDATE   "22 Oct 2003"
-#define LSMDATE "22Oct03"
+#define BDATE   "24 Oct 2003"
+#define LSMDATE "24Oct03"
 
 /* Debug flags */
 #undef  DEBUG
