576da01168ace4be28f57e84299f915e39b04048
Kern Sibbald <kern@sibbald.com>
2004-01-29 20:59:42 +0000

    Fix command line argument handling in restore
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@1024 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -2,8 +2,8 @@
 #undef  VERSION
 #define VERSION "1.33"
 #define VSTRING "1"
-#define BDATE   "26 Jan 2004"
-#define LSMDATE "26Jan04"
+#define BDATE   "29 Jan 2004"
+#define LSMDATE "29Jan04"
 
 /* Debug flags */
 #undef  DEBUG
