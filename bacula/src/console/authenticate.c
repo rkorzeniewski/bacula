108f4ef89086484542f98f62dcb7d8c3be29070f
Kern Sibbald <kern@sibbald.com>
2004-04-13 12:50:27 +0000

    Add wx-console directory
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@1194 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/console/authenticate.c
+++ bacula/src/console/authenticate.c
@@ -11,7 +11,7 @@
  *
  */
 /*
-   Copyright (C) 2000, 2001 Kern Sibbald and John Walker
+   Copyright (C) 2001-2004 Kern Sibbald and John Walker
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -23,9 +23,10 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+   You should have received a copy of the GNU General Public
+   License along with this program; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, 
+   MA 02111-1307, USA.
  */
 
 #include "bacula.h"
