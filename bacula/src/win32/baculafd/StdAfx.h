3a2dc4745e586a7766e44f0e1191580d4a080ac8
Nicolas Boichat <nicolas@boichat.ch>
2004-04-23 20:14:59 +0000

    Commented out MFC include files, which are not distributed with the free version of VC++, and which are not used in bacula-fd.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@1276 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/win32/baculafd/StdAfx.h
+++ bacula/src/win32/baculafd/StdAfx.h
@@ -10,16 +10,18 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
-#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+#define VC_EXTRALEAN        // Exclude rarely-used stuff from Windows headers
 
-#include <afx.h>
+/* Commented out MFC include files, which are not distributed with the free
+ * version of VC++, and which are not used in bacula-fd. */
+/*#include <afx.h>
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
-#include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
+#include <afxdtctl.h>       // MFC support for Internet Explorer 4 Common Controls
 #ifndef _AFX_NO_AFXCMN_SUPPORT
-#include <afxcmn.h>			// MFC support for Windows Common Controls
+#include <afxcmn.h>         // MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
-
+*/
 #include <iostream>
 
 // TODO: reference additional headers your program requires here
