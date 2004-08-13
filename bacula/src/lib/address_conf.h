febc09804eb2fe5f2805c8b36c533173846bdbde
Kern Sibbald <kern@sibbald.com>
2004-08-13 09:35:53 +0000

    Undo a few changes in address_conf.h and smartalloc.h so that it works here without IP6 defined
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@1519 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/lib/address_conf.h
+++ bacula/src/lib/address_conf.h
@@ -26,7 +26,8 @@
  */
 
 
-class IPADDR : public SMARTALLOC {
+//class IPADDR : public SMARTALLOC {
+class IPADDR {
  public:
    typedef enum { R_SINGLE, R_SINGLE_PORT, R_SINGLE_ADDR, R_MULTIPLE,
                   R_DEFAULT, R_EMPTY
