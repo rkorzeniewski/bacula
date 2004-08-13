8d8608316d7b27026bd115f24c73facd5af12043
Nicolas Boichat <nicolas@boichat.ch>
2004-08-13 21:51:38 +0000

    - wxbHistoryTextCtrl : Fixed Win32 build error
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@1524 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/wx-console/wxbhistorytextctrl.h
+++ bacula/src/wx-console/wxbhistorytextctrl.h
@@ -34,32 +34,33 @@
 #endif
 
 #include <wx/treectrl.h>
+#include <wx/hashmap.h>
 
 WX_DECLARE_STRING_HASH_MAP( wxString, wxbCommands );
 
 class wxbHistoryTextCtrl: public wxTextCtrl {
    public:
-      wxbHistoryTextCtrl(wxStaticText* help, wxWindow* parent, wxWindowID id, 
+      wxbHistoryTextCtrl(wxStaticText* help, wxWindow* parent, wxWindowID id,
          const wxString& value = "", const wxPoint& pos = wxDefaultPosition,
-         const wxSize& size = wxDefaultSize, 
-         const wxValidator& validator = wxDefaultValidator, 
+         const wxSize& size = wxDefaultSize,
+         const wxValidator& validator = wxDefaultValidator,
          const wxString& name = wxTextCtrlNameStr);
-         
+
       void HistoryAdd(wxString cmd);
-      
+
       void AddCommand(wxString cmd, wxString description);
       void ClearCommandList();
-      
+
       virtual void SetValue(const wxString& value);
    private:
       wxArrayString history;
       wxbCommands commands;
       int index;
       wxStaticText* help;
-      
+
       void OnKeyUp(wxKeyEvent& event);
       void OnKeyDown(wxKeyEvent& event);
-      
+
       DECLARE_EVENT_TABLE();
 };
 
