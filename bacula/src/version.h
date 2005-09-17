bcc56c7f9bf455814723292435f46e31ec733537
Kern Sibbald <kern@sibbald.com>
2005-09-17 16:40:15 +0000

    Make new_volume walk through all Vols looking for dev   to release.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@2394 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "1.37.39"
-#define BDATE   "16 September 2005"
-#define LSMDATE "16Sep05"
+#define BDATE   "17 September 2005"
+#define LSMDATE "17Sep05"
 
 /* Debug flags */
 #undef  DEBUG
