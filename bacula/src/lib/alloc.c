156a0bf9b6180342d3cacfca5c601dc6d5f0842d
Kern Sibbald <kern@sibbald.com>
2006-05-01 08:18:44 +0000

    - Work on getting wx-console building on MinGW. wxWidgets now builds   started adding Makefile ... - Moved MinGW library objects to src/win32/lib to reduce clutter.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@2991 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/lib/alloc.c
+++ bacula/src/lib/alloc.c
@@ -1,27 +1,22 @@
 /*
 
-	Error checking memory allocator
+        Error checking memory allocator
 
      Version $Id$
 */
 
 /*
-   Copyright (C) 2000-2004 Kern Sibbald and John Walker
+   Copyright (C) 2000-2006 Kern Sibbald
 
    This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of
-   the License, or (at your option) any later version.
+   modify it under the terms of the GNU General Public License
+   version 2 as amended with additional clauses defined in the
+   file LICENSE in the main source directory.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public
-   License along with this program; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-   MA 02111-1307, USA.
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+   the file LICENSE for additional details.
 
  */
 
@@ -37,7 +32,7 @@
 
 /*LINTLIBRARY*/
 
-#define V	 (void)
+#define V        (void)
 
 #ifdef SMARTALLOC
 
@@ -47,16 +42,16 @@ extern void *sm_malloc();
 
 void *sm_alloc(char *fname, int lineno, unsigned int nbytes)
 {
-	void *buf;
+   void *buf;
 
-	if ((buf = sm_malloc(fname, lineno, nbytes)) != NULL) {
-	   return buf;
-	}
-	V fprintf(stderr, "\nBoom!!!  Memory capacity exceeded.\n");
-	V fprintf(stderr, "  Requested %u bytes at line %d of %s.\n",
-	   nbytes, lineno, fname);
-	abort();
-	/*NOTREACHED*/
+   if ((buf = sm_malloc(fname, lineno, nbytes)) != NULL) {
+      return buf;
+   }
+   V fprintf(stderr, "\nBoom!!!  Memory capacity exceeded.\n");
+   V fprintf(stderr, "  Requested %u bytes at line %d of %s.\n",
+      nbytes, lineno, fname);
+   abort();
+   /*NOTREACHED*/
 }
 #else
 
@@ -64,15 +59,15 @@ void *sm_alloc(char *fname, int lineno, unsigned int nbytes)
 
 void *alloc(unsigned int nbytes)
 {
-	void *buf;
+   void *buf;
 
-	if ((buf = malloc(nbytes)) != NULL) {
-	   return buf;
-	}
-	V fprintf(stderr, "\nBoom!!!  Memory capacity exceeded.\n");
-	V fprintf(stderr, "  Requested %u bytes.\n", nbytes);
-	abort();
-	/*NOTREACHED*/
+   if ((buf = malloc(nbytes)) != NULL) {
+      return buf;
+   }
+   V fprintf(stderr, "\nBoom!!!  Memory capacity exceeded.\n");
+   V fprintf(stderr, "  Requested %u bytes.\n", nbytes);
+   abort();
+   /*NOTREACHED*/
 }
 #endif
 #endif
