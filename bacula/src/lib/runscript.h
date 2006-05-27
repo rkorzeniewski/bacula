f88513ccb9b4b46e86326fb9ec343a0dee2fb3dc
Eric Bollengier <eric@eb.homelinux.org>
2006-05-27 17:24:10 +0000

    o initial add
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@3035 91ce42f0-d328-0410-95d8-f526ca767f89

---
+++ bacula/src/lib/runscript.h
@@ -0,0 +1,94 @@
+/*
+ * Bacula RUNSCRIPT Structure definition for FileDaemon and Director
+ * Eric Bollengier May 2006
+ * Version $Id$
+ */
+/*
+   Copyright (C) 2000-2006 Kern Sibbald
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   version 2 as amended with additional clauses defined in the
+   file LICENSE in the main source directory.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+   the file LICENSE for additional details.
+
+ */
+
+
+#ifndef __RUNSCRIPT_H_
+#define __RUNSCRIPT_H_ 1
+
+#if defined(FILE_DAEMON) || defined(DIRECTOR_DAEMON)
+# define USE_RUNSCRIPT
+#endif
+
+#ifdef USE_RUNSCRIPT
+
+/* Usage:
+ *
+ * #define USE_RUNSCRIPT
+ * #include "lib/runscript.h"
+ * 
+ * RUNSCRIPT *script = new_runscript();
+ * script->set_command("/bin/sleep 20");
+ * script->on_failure = true;
+ * script->when = SCRIPT_After;
+ * 
+ * script->run("Label");
+ * free_runscript(script);
+ */
+
+/* 
+ * RUNSCRIPT->when can take following value : 
+ */
+enum {
+   SCRIPT_Never  = 0,
+   SCRIPT_After  = 1,		/* AfterJob */
+   SCRIPT_Before = 2,		/* BeforeJob */
+   SCRIPT_Any    = 3           	/* Before and After */
+};
+
+/*
+ * Structure for RunScript ressource
+ */
+class RUNSCRIPT {
+public:
+   POOLMEM *command;		/* command string */
+   POOLMEM *target;		/* host target */
+   char level;			/* Base|Full|Incr...|All (NYI) */
+   bool on_success;		/* executre command on job success (After) */
+   bool on_failure;             /* executre command on job failure (After) */
+   bool abort_on_error;         /* abort job on error (Before) */
+   int  when;                   /* SCRIPT_Before|Script_After BEFORE/AFTER JOB*/
+
+   int run(JCR *job, const char *name="");
+   bool can_run_at_level(int JobLevel) { return true;};        /* TODO */
+   void set_command(const POOLMEM *cmd);
+   void set_target(const POOLMEM *client_name);
+   void reset_default(bool free_string = false);
+   bool is_local();		/* true if running on local host */
+   void debug();
+};
+
+/* create new RUNSCRIPT (set all value to 0) */
+RUNSCRIPT *new_runscript();           
+
+/* create new RUNSCRIPT from an other */
+RUNSCRIPT *copy_runscript(RUNSCRIPT *src);
+
+/* launch each script from runscripts*/
+int run_scripts(JCR *jcr, alist *runscripts, const char *name); 
+
+/* free RUNSCRIPT (and all POOLMEM) */
+void free_runscript(RUNSCRIPT *script);
+
+/* foreach_alist free RUNSCRIPT */
+void free_runscripts(alist *runscripts); /* you have to free alist */
+
+#endif /* USE_RUNSCRIPT */
+
+#endif /* __RUNSCRIPT_H_ */
