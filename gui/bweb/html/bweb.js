e28e7eb97c08f987eee2f2b966a98cb1f82fce9d
Eric Bollengier <eric@eb.homelinux.org>
2006-08-16 17:27:14 +0000

    ebl  Add pool view
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@3287 91ce42f0-d328-0410-95d8-f526ca767f89

--- gui/bweb/html/bweb.js
+++ gui/bweb/html/bweb.js
@@ -53,3 +53,73 @@ function bweb_add_refresh(){
 	window.setInterval("bweb_refresh()",refresh_time);
 }
 
+//
+// percent_display("row2", [ { nb: 1, name: "Full"   },
+//			   { nb: 2, name: "Error"  },
+//			   { nb: 5, name: "Append" },
+//			   { nb: 2, name: "Purged" },
+//                         {}                               # last element must be {}
+//		         ]);
+
+function percent_get_img(type)
+{
+   var img=document.createElement('img');
+   if (type) {
+      img.className="pSlice" + type ;
+   } else {
+      img.className="pSlice";
+   }
+   img.src="/bweb/pix.png";
+   img.alt="";
+   return img;
+}
+
+function percent_display(parent, hash_values)
+{
+   var nb_elt=20;
+   var tips= "";
+
+   if (typeof parent != "object") {
+      parent = document.getElementById(parent);
+   } 
+
+   if (!parent) {
+       alert("E : display_percent(): Can't find parent " + parent);
+       return;
+   }
+
+   hash_values.pop(); // drop last element {}
+
+   var nb_displayed = 0;
+   var nb_max = 0;
+
+   for(var i=0;i<hash_values.length;i++) {
+        nb_max += hash_values[i]['nb'];
+   }
+
+   for(var i=0;i<hash_values.length;i++) {
+        var elt = hash_values[i];
+        var cur_nb = (elt['nb'] * nb_elt)/nb_max;
+        var cur_name = elt['name'];
+        cur_name.replace(/-/,"_");
+
+        tips = tips + " " + elt['nb'] + " " + cur_name;
+
+        while ((nb_displayed < nb_elt) && (cur_nb >=1)) {
+            nb_displayed++;
+            cur_nb--;
+
+            var img= percent_get_img(cur_name);
+            parent.appendChild(img);
+        }       
+   }
+
+   while (nb_displayed < nb_elt) {
+      nb_displayed++;
+      var img= percent_get_img();
+      parent.appendChild(img);
+  }     
+
+  parent.title = tips;
+
+}
