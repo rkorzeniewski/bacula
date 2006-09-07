f8f848fdb5aace95292d905dc60fe0de45521859
Kern Sibbald <kern@sibbald.com>
2006-09-07 11:52:20 +0000

    kes  Apply patch supplied in bug #656 to pass priority field      in the run dialog to the Director. kes  Restore DCR after VOL_NAME_ERROR in mount.c.  Hopefully      this will fix bug #627 where Bacula eventually gets      confused about what Volume it wants.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@3425 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "1.39.22"
-#define BDATE   "06 Septermber 2006"
-#define LSMDATE "06Sep06"
+#define BDATE   "07 Septermber 2006"
+#define LSMDATE "07Sep06"
 #define BYEAR "2006"       /* year for copyright messages in progs */
 
 /* Debug flags */
