1ee7f23c03673e46bd30aac9c585b2045719a34f
Kern Sibbald <kern@sibbald.com>
2006-11-13 18:00:58 +0000

    kes  Replace () by {} in configure.in for proper HP configuration. kes  Shorten some lines over 80 characters in filed/backup.c kes  Cast sizeof() in filed/backup.c to avoid compiler warning.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@3613 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -3,7 +3,7 @@
  */
 
 #undef  VERSION
-#define VERSION "1.39.28"
+#define VERSION "1.39.29"
 #define BDATE   "12 November 2006"
 #define LSMDATE "12Nov06"
 #define BYEAR "2006"       /* year for copyright messages in progs */
