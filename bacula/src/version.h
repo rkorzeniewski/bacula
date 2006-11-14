0edee6863d38faf09f3de2d08b2bac730430612d
Kern Sibbald <kern@sibbald.com>
2006-11-14 21:06:39 +0000

    Update version date
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@3624 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "1.39.29"
-#define BDATE   "12 November 2006"
-#define LSMDATE "12Nov06"
+#define BDATE   "14 November 2006"
+#define LSMDATE "14Nov06"
 #define BYEAR "2006"       /* year for copyright messages in progs */
 
 /* Debug flags */
