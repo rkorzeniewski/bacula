6bbe63544183bb16e1e842911d153869fbe51827
Landon Fuller <landonf@opendarwin.org>
2006-12-09 23:50:00 +0000

    This commit fixes backup and restore of HFS+ resource forks. Unrelated to the above, I added a long-missing 'break' statement in HFS+ attribute handling that resulted in restore failures.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@3785 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/filed/backup.c
+++ bacula/src/filed/backup.c
@@ -462,6 +462,7 @@ static int save_file(FF_PKT *ff_pkt, void *vjcr, bool top_level)
             ff_pkt->flags & FO_HFSPLUS)) {
       if (ff_pkt->hfsinfo.rsrclength > 0) {
          int flags;
+	 int rsrc_stream;
          if (!bopen_rsrc(&ff_pkt->bfd, ff_pkt->fname, O_RDONLY | O_BINARY, 0) < 0) {
             ff_pkt->ff_errno = errno;
             berrno be;
@@ -475,7 +476,12 @@ static int save_file(FF_PKT *ff_pkt, void *vjcr, bool top_level)
          }
          flags = ff_pkt->flags;
          ff_pkt->flags &= ~(FO_GZIP|FO_SPARSE);
-         stat = send_data(jcr, STREAM_MACOS_FORK_DATA, ff_pkt, digest, signing_digest);
+	 if (flags & FO_ENCRYPT) {
+	    rsrc_stream = STREAM_ENCRYPTED_MACOS_FORK_DATA;
+	 } else {
+	    rsrc_stream = STREAM_MACOS_FORK_DATA;
+	 }
+	 stat = send_data(jcr, rsrc_stream, ff_pkt, digest, signing_digest);
          ff_pkt->flags = flags;
          bclose(&ff_pkt->bfd);
          if (!stat) {
