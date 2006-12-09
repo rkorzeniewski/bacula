11c9cdd422f9159ad08555123cda59e9ad07ce58
Eric Bollengier <eric@eb.homelinux.org>
2006-12-09 14:25:02 +0000

    ebl  update copyright
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@3778 91ce42f0-d328-0410-95d8-f526ca767f89

--- gui/brestore/brestore.pl
+++ gui/brestore/brestore.pl
@@ -31,25 +31,36 @@ my $glade_file = 'brestore.glade' ;
 
 =head1 COPYRIGHT
 
-  Copyright (C) 2006 Marc Cousin and Eric Bollengier
+   Bacula® - The Network Backup Solution
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2 of the License, or (at your option) any later version.
- 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-  
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the
-  Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-  Boston, MA 02111-1307, USA.
-  
-  Base 64 functions from Karl Hakimian <hakimian@aha.com>
-  Integrally copied from recover.pl from bacula source distribution.
+   Copyright (C) 2000-2006 Free Software Foundation Europe e.V.
+
+   Brestore authors are Marc Cousin and Eric Bollengier.
+   The main author of Bacula is Kern Sibbald, with contributions from
+   many others, a complete list can be found in the file AUTHORS.
+
+   This program is Free Software; you can redistribute it and/or
+   modify it under the terms of version two of the GNU General Public
+   License as published by the Free Software Foundation plus additions
+   that are listed in the file LICENSE.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+
+   Bacula® is a registered trademark of John Walker.
+   The licensor of Bacula is the Free Software Foundation Europe
+   (FSFE), Fiduciary Program, Sumatrastrasse 25, 8006 Zurich,
+   Switzerland, email:ftf@fsfeurope.org.
+
+   Base 64 functions from Karl Hakimian <hakimian@aha.com>
+   Integrally copied from recover.pl from bacula source distribution.
 
 =cut
 
