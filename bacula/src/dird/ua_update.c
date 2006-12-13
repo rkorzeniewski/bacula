c53c0b56ea6a8ee2065eb6ea762123b9acb3deb4
Eric Bollengier <eric@eb.homelinux.org>
2006-12-13 10:35:04 +0000

    ebl  revert "Add update volume=xxx status=Purged"      setting a volume to purged should only be done by the purge command.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@3796 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/dird/ua_update.c
+++ bacula/src/dird/ua_update.c
@@ -114,7 +114,6 @@ static void update_volstatus(UAContext *ua, const char *val, MEDIA_DBR *mr)
       NT_("Recycle"),
       NT_("Read-Only"),
       NT_("Error"),
-      NT_("Purged"),
       NULL};
    bool found = false;
    int i;
