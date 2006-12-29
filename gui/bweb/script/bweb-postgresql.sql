ee3facec08693989c8e02541f017d8d740c7468d
Eric Bollengier <eric@eb.homelinux.org>
2006-12-29 11:41:33 +0000

    ebl  add function to decode base64 lstat
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@3855 91ce42f0-d328-0410-95d8-f526ca767f89

--- gui/bweb/script/bweb-postgresql.sql
+++ gui/bweb/script/bweb-postgresql.sql
@@ -20,4 +20,21 @@ RETURNS double precision AS $$
     select extract(epoch from $1);
 $$ LANGUAGE SQL;
 
+CREATE OR REPLACE FUNCTION base64_decode_lstat(int4, varchar) RETURNS int8 AS $$
+DECLARE
+val int8;
+b64 varchar(64);
+size varchar(64);
+i int;
+BEGIN
+size := split_part($2, ' ', $1);
+b64 := 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+val := 0;
+FOR i IN 1..length(size) LOOP
+val := val + (strpos(b64, substr(size, i, 1))-1) * (64^(length(size)-i));
+END LOOP;
+RETURN val;
+END;
+$$ language 'plpgsql';
+
 COMMIT;
