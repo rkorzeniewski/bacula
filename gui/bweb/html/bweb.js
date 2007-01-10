0e18fd5857c39bc14ce866d383bb0196f35e3026
Eric Bollengier <eric@eb.homelinux.org>
2007-01-10 07:59:31 +0000

    ebl  update copyright
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@3960 91ce42f0-d328-0410-95d8-f526ca767f89

--- gui/bweb/html/bweb.js
+++ gui/bweb/html/bweb.js
@@ -1,19 +1,31 @@
-// Copyright (C) 2006 Eric Bollengier
-//     All rights reserved.
+// Bweb - A Bacula web interface
+// Bacula® - The Network Backup Solution
 //
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// any later version.
+// Copyright (C) 2000-2006 Free Software Foundation Europe e.V.
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// The main author of Bweb is Eric Bollengier.
+// The main author of Bacula is Kern Sibbald, with contributions from
+// many others, a complete list can be found in the file AUTHORS.
+//
+// This program is Free Software; you can redistribute it and/or
+// modify it under the terms of version two of the GNU General Public
+// License as published by the Free Software Foundation plus additions
+// that are listed in the file LICENSE.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301, USA.
+//
+// Bacula® is a registered trademark of John Walker.
+// The licensor of Bacula is the Free Software Foundation Europe
+// (FSFE), Fiduciary Program, Sumatrastrasse 25, 8006 Zurich,
+// Switzerland, email:ftf@fsfeurope.org.
 
  var even_cell_color = "#FFFFFF";
  var odd_cell_color  = "#EEEEEE";
