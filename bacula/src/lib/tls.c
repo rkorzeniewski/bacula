f4324a5f7ef2fe3fbeae120e4382074dda375a83
Kern Sibbald <kern@sibbald.com>
2007-02-06 14:36:28 +0000

    Update a few dates
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@4115 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/lib/tls.c
+++ bacula/src/lib/tls.c
@@ -22,7 +22,7 @@
 /*
    Bacula® - The Network Backup Solution
 
-   Copyright (C) 2005-2006 Free Software Foundation Europe e.V.
+   Copyright (C) 2005-2007 Free Software Foundation Europe e.V.
 
    The main author of Bacula is Kern Sibbald, with contributions from
    many others, a complete list can be found in the file AUTHORS.
