beee801c08bf1f207fe5e00f6dc2bde712dd1f8c
Kern Sibbald <kern@sibbald.com>
2007-02-08 10:54:40 +0000

    kes  Fix dird/ua_cmds.c so that a cancel command checks if the      console is authorized to cancel the job.  This fixes bug      #767. kes  Modify SD so that the VolCatJobs medium record is updated      at the beginning of a Job rather than the end.  This      fixes bug #775 where exceeding MaxVolJobs caused jobs      to fail. kes  Added a mutex around getting and setting Volume information      so that multiple simultaneous jobs will single thread.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@4145 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "2.1.2"
-#define BDATE   "07 February 2007"
-#define LSMDATE "07Feb07"
+#define BDATE   "08 February 2007"
+#define LSMDATE "08Feb07"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2007 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2007"       /* year for copyright messages in progs */
