a4ffa7424b54401e8d83d6b6269c6aaeb97075dc
Kern Sibbald <kern@sibbald.com>
2007-02-16 17:11:07 +0000

    Tweak
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@4189 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/filed/restore.c
+++ bacula/src/filed/restore.c
@@ -1,15 +1,7 @@
 /*
- *  Bacula File Daemon  restore.c Restorefiles.
- *
- *    Kern Sibbald, November MM
- *
- *   Version $Id$
- *
- */
-/*
    Bacula® - The Network Backup Solution
 
-   Copyright (C) 2000-2006 Free Software Foundation Europe e.V.
+   Copyright (C) 2000-2007 Free Software Foundation Europe e.V.
 
    The main author of Bacula is Kern Sibbald, with contributions from
    many others, a complete list can be found in the file AUTHORS.
@@ -33,6 +25,14 @@
    (FSFE), Fiduciary Program, Sumatrastrasse 25, 8006 Zürich,
    Switzerland, email:ftf@fsfeurope.org.
 */
+/*
+ *  Bacula File Daemon  restore.c Restorefiles.
+ *
+ *    Kern Sibbald, November MM
+ *
+ *   Version $Id$
+ *
+ */
 
 #include "bacula.h"
 #include "filed.h"
