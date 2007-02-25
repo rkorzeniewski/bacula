3318fdece6f7b7fda26621c0597ac6bbfb48fb1a
Eric Bollengier <eric@eb.homelinux.org>
2007-02-25 16:40:01 +0000

    ebl  init
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@4252 91ce42f0-d328-0410-95d8-f526ca767f89

---
+++ gui/bweb/script/update_job_old.sql
@@ -0,0 +1,8 @@
+-- First, create job_old table
+-- CREATE TABLE job_old (LIKE Job);
+-- 
+-- then put this on your crontab
+-- */20 * * * * psql -f /opt/bacula/etc/update_job_old.sql > /home/bacula/update_job_old.log
+INSERT INTO job_old
+  (SELECT * FROM Job WHERE JobId NOT IN (SELECT JobId FROM job_old) );
+
