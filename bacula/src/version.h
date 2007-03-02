21b7c18e147f584cf0c2f836266b432856b41980
Kern Sibbald <kern@sibbald.com>
2007-03-02 18:02:46 +0000

    02Mar07 kes  Modify jobq.c to let simultaneous restore jobs run. This may      cause them to fail if they cannot get a drive. kes  Add Client Connect Wait to Storage daemon to permit users to      modify the time the SD waits for a FD connection.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@4294 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "2.1.4"
-#define BDATE   "21 February 2007"
-#define LSMDATE "21Feb07"
+#define BDATE   "02 March 2007"
+#define LSMDATE "02Mar07"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2007 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2007"       /* year for copyright messages in progs */
