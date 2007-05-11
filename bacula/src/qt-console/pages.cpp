91cb329be06cceed885b6e06ca466fd5d8765c5c
Kern Sibbald <kern@sibbald.com>
2007-05-11 18:41:25 +0000

    Turn off bat debug code
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@4749 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/qt-console/pages.cpp
+++ bacula/src/qt-console/pages.cpp
@@ -217,10 +217,12 @@ void Pages::consoleCommand(QString &command)
        return;
    /* Bring this directors console to the front of the stack */
    setConsoleCurrent();
+#ifdef xxdebugcode
    m_console->display_text("Context sensitive command :\n\n");
    m_console->display_text("****    ");
    m_console->display_text(command + "    ****\n");
    m_console->display_text("Director Response :\n\n");
+#endif
    m_console->write_dir(command.toUtf8().data());
    m_console->displayToPrompt();
 }
