d7522c18b809a8047d01d03b2b0009510911fa79
Kern Sibbald <kern@sibbald.com>
2007-06-11 10:49:48 +0000

    10Jun08 kes  Move find_next_appendable_volume() to after acquiring a valid device      in reserve.c.  This fixes bug #864 -- confirmed by reporter. 09Jun07 kes  Unable to reproduce bug 872. However added additional testing for      integer. kes  Bug #874 resolved by documenting SQLite limitations. kes  Bug #882 resolved by documenting mtx-changer script for new drive code. kes  Bug #881 resolved by removing typo in mtx-changer script. kes  Made Frank Sweetser's wiki the official Bacula wiki with his      approval. Added a link to the wiki on the web page, and implemented      a backup from http://paramount.ind.wpi.edu 08Jun07 kes  Fix update allfrompool to pass pool name
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@5006 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -3,9 +3,9 @@
  */
 
 #undef  VERSION
-#define VERSION "2.1.13"
-#define BDATE   "08 June 2007"
-#define LSMDATE "08Jun07"
+#define VERSION "2.1.14"
+#define BDATE   "10 June 2007"
+#define LSMDATE "10Jun07"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2007 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2007"       /* year for copyright messages in progs */
