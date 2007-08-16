9e1f96b1eb7a5b6d61f9dc409dab1c1bc5408477
Dan Langille <dan@langille.org>
2007-08-16 18:46:16 +0000

    Don't set our PID after we drop privs.  Do it before.  This fixes bug #923
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@5362 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/dird/dird.c
+++ bacula/src/dird/dird.c
@@ -228,6 +228,15 @@ int main (int argc, char *argv[])
       Jmsg((JCR *)NULL, M_ERROR_TERM, 0, _("Please correct configuration file: %s\n"), configfile);
    }
 
+   if (background) {
+      daemon_start();
+      init_stack_dump();              /* grab new pid */
+   }
+
+   /* Create pid must come after we are a daemon -- so we have our final pid */
+   create_pid_file(director->pid_directory, "bacula-dir", get_first_port_host_order(director->DIRaddrs));
+   read_state_file(director->working_directory, "bacula-dir", get_first_port_host_order(director->DIRaddrs));
+
    drop(uid, gid);                    /* reduce privileges if requested */
 
    if (!check_catalog()) {
@@ -246,15 +255,6 @@ int main (int argc, char *argv[])
 
    FDConnectTimeout = (int)director->FDConnectTimeout;
    SDConnectTimeout = (int)director->SDConnectTimeout;
-
-   if (background) {
-      daemon_start();
-      init_stack_dump();              /* grab new pid */
-   }
-
-   /* Create pid must come after we are a daemon -- so we have our final pid */
-   create_pid_file(director->pid_directory, "bacula-dir", get_first_port_host_order(director->DIRaddrs));
-   read_state_file(director->working_directory, "bacula-dir", get_first_port_host_order(director->DIRaddrs));
 
 
 #if !defined(HAVE_WIN32)
