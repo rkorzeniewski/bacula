78d1ff263d69d517dd715add096db56501fc634e
Eric Bollengier <eric@eb.homelinux.org>
2007-11-18 16:32:25 +0000

    ebl  Add view media role
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@5957 91ce42f0-d328-0410-95d8-f526ca767f89

--- gui/bweb/script/bweb-postgresql.sql
+++ gui/bweb/script/bweb-postgresql.sql
@@ -55,6 +55,7 @@ INSERT INTO bweb_role (rolename) VALUES ('r_prune');
 INSERT INTO bweb_role (rolename) VALUES ('r_purge');
 
 INSERT INTO bweb_role (rolename) VALUES ('r_view_job');
+INSERT INTO bweb_role (rolename) VALUES ('r_view_media');
 INSERT INTO bweb_role (rolename) VALUES ('r_view_stat');
 
 INSERT INTO bweb_role (rolename) VALUES ('r_run_job');
--- gui/bweb/script/upgrade-2.2_3.0_mysql.sql
+++ gui/bweb/script/upgrade-2.2_3.0_mysql.sql
@@ -34,6 +34,7 @@ INSERT INTO bweb_role (rolename) VALUES ('r_purge');
 
 INSERT INTO bweb_role (rolename) VALUES ('r_view_job');
 INSERT INTO bweb_role (rolename) VALUES ('r_view_stat');
+INSERT INTO bweb_role (rolename) VALUES ('r_view_media');
 
 INSERT INTO bweb_role (rolename) VALUES ('r_run_job');
 INSERT INTO bweb_role (rolename) VALUES ('r_cancel_job');
