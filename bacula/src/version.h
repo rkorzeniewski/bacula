20144fe2b1d7987445fb6074ffe6eba440ff74bc
Kern Sibbald <kern@sibbald.com>
2008-02-14 13:22:35 +0000

    Temp fix to plugin name file_index.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@6419 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -3,9 +3,9 @@
  */
 
 #undef  VERSION
-#define VERSION "2.3.9"
-#define BDATE   "10 February 2008"
-#define LSMDATE "10Feb08"
+#define VERSION "2.3.10"
+#define BDATE   "14 February 2008"
+#define LSMDATE "14Feb08"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2008 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2008"       /* year for copyright messages in progs */
