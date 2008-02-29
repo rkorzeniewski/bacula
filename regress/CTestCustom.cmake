5e38485dbd006fcc5a15d1331f3a24c40acac401
Kern Sibbald <kern@sibbald.com>
2008-02-29 10:18:37 +0000

    Commit Frank's patch
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@6522 91ce42f0-d328-0410-95d8-f526ca767f89

---
+++ regress/CTestCustom.cmake
@@ -0,0 +1,11 @@
+SET(CTEST_CUSTOM_ERROR_EXCEPTION
+  ${CTEST_CUSTOM_ERROR_EXCEPTION}
+  "ERROR: *database \".*\" already exists"
+  "ERROR: *table \".*\" does not exist"
+  "NOTICE: .*will create implicit sequence"
+  "NOTICE: .*will create implicit index"
+  "ERROR: *role \".*\" already exists"
+  )
+
+SET(CTEST_CUSTOM_MAXIMUM_PASSED_TEST_OUTPUT_SIZE 1048576)
+SET(CTEST_CUSTOM_MAXIMUM_FAILED_TEST_OUTPUT_SIZE 1048576)
