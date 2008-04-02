555e40369e40e077288e19c6d826a2b920ccf465
Kern Sibbald <kern@sibbald.com>
2008-04-02 20:37:26 +0000

    02Apr08 kes  Apply patch from bug #1069 that corrects spurious error messages      when ACLs enabled on SGI but no ACL exists.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@6729 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -3,9 +3,9 @@
  */
 
 #undef  VERSION
-#define VERSION "2.3.14"
-#define BDATE   "01 April 2008"
-#define LSMDATE "01Apr08"
+#define VERSION "2.3.15"
+#define BDATE   "02 April 2008"
+#define LSMDATE "02Apr08"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2008 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2008"       /* year for copyright messages in progs */
