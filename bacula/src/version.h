c29a6ceae9db93fc999c4a3450f3b49f612320f4
Kern Sibbald <kern@sibbald.com>
2008-05-03 09:01:28 +0000

    kes  Implement regression that explicitly tests swapping a Volume      from one drive to another. kes  Enhance disk-changer to detect most error conditions. kes  Fix SD code so that it properly swaps a Volume between drives.      This fixes bug #1083.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@6881 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -3,9 +3,9 @@
  */
 
 #undef  VERSION
-#define VERSION "2.3.19"
-#define BDATE   "02 May 2008"
-#define LSMDATE "02May08"
+#define VERSION "2.3.20"
+#define BDATE   "03 May 2008"
+#define LSMDATE "03May08"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2008 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2008"       /* year for copyright messages in progs */
