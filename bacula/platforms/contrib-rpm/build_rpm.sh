e0bae75dc99f5de75044e8b53db6ba02cd733b46
Scott Barninger <scott@barninger.com>
2008-05-24 12:05:42 +0000

    Add Fedora 9 target.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@7019 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/platforms/contrib-rpm/build_rpm.sh
+++ bacula/platforms/contrib-rpm/build_rpm.sh
@@ -25,11 +25,11 @@ VERSION=2.2.8
 RELEASE=1
 
 # build platform for spec
-# set to one of rh7,rh8,rh9,fc1,fc3,fc4,fc5,fc6,fc7,fc8,wb3,rhel3,rhel4,rhel5,centos3,centos4,centos5,sl3, sl4,sl5,su9,su10,su102,su103,mdk,mdv
+# set to one of rh7,rh8,rh9,fc1,fc3,fc4,fc5,fc6,fc7,fc8,fc9,wb3,rhel3,rhel4,rhel5,centos3,centos4,centos5,sl3, sl4,sl5,su9,su10,su102,su103,mdk,mdv
 PLATFORM=su102
 
 # platform designator for file names
-# for RedHat/Fedora set to one of rh7,rh8,rh9,fc1,fc3,fc4,fc5,fc6,fc7,fc8 OR
+# for RedHat/Fedora set to one of rh7,rh8,rh9,fc1,fc3,fc4,fc5,fc6,fc7,fc8,fc9 OR
 # for RHEL3/clones wb3, rhel3, sl3 & centos3 set to el3 OR
 # for RHEL4/clones rhel4, sl4 & centos4 set to el4 OR
 # for RHEL5/clones rhel5, sl5 & centos5 set to el5 OR
@@ -183,3 +183,4 @@ ls
 # 15 Sep 2007 add rhel5 and clones
 # 10 Nov 2007 add su103
 # 12 Jan 2008 add fc8
+# 23 May 2008 add fc9
