43909e0c4f57630b4784e12b2c0d4a5a6998e4c3
Kern Sibbald <kern@sibbald.com>
2008-06-01 08:48:28 +0000

    Add hostname to traceback
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@7082 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "2.3.22"
-#define BDATE   "30 May 2008"
-#define LSMDATE "30May08"
+#define BDATE   "01 June 2008"
+#define LSMDATE "01Jun08"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2008 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2008"       /* year for copyright messages in progs */
