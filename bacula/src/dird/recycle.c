5b83ee0d1827ae8a24939b22d2ae69abf2696c97
Eric Bollengier <eric@eb.homelinux.org>
2008-06-25 13:14:02 +0000

    ebl  Fix find_next_volume_for_append logic
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@7228 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/dird/recycle.c
+++ bacula/src/dird/recycle.c
@@ -40,23 +40,6 @@
 #include "dird.h"
 #include "ua.h"
 
-struct s_oldest_ctx {
-   uint32_t MediaId;
-   char LastWritten[30];
-};
-
-static int oldest_handler(void *ctx, int num_fields, char **row)
-{
-   struct s_oldest_ctx *oldest = (struct s_oldest_ctx *)ctx;
-
-   if (row[0]) {
-      oldest->MediaId = str_to_int64(row[0]);
-      bstrncpy(oldest->LastWritten, row[1]?row[1]:"", sizeof(oldest->LastWritten));
-      Dmsg1(100, "New oldest %s\n", row[1]?row[1]:"");
-   }
-   return 1;
-}
-
 /* Forward referenced functions */
 
 bool find_recycled_volume(JCR *jcr, bool InChanger, MEDIA_DBR *mr)
@@ -71,43 +54,18 @@ bool find_recycled_volume(JCR *jcr, bool InChanger, MEDIA_DBR *mr)
    return false;
 }
 
-
 /*
  *   Look for oldest Purged volume
  */
 bool recycle_oldest_purged_volume(JCR *jcr, bool InChanger, MEDIA_DBR *mr)
 {
-   struct s_oldest_ctx oldest;
-   char ed1[50];
-   POOLMEM *query = get_pool_memory(PM_EMSG);
-   const char *select =
-          "SELECT MediaId,LastWritten FROM Media "
-          "WHERE PoolId=%s AND Recycle=1 AND VolStatus='Purged' "
-          "AND Enabled=1 AND MediaType='%s' "
-          "ORDER BY LastWritten ASC,MediaId LIMIT 1";
-
-   Dmsg0(100, "Enter recycle_oldest_purged_volume\n");
-   oldest.MediaId = 0;
-   Mmsg(query, select, edit_int64(mr->PoolId, ed1), mr->MediaType);
-
-   if (!db_sql_query(jcr->db, query, oldest_handler, (void *)&oldest)) {
-      Jmsg(jcr, M_ERROR, 0, "%s", db_strerror(jcr->db));
-      Dmsg0(100, "return 0  recycle_oldest_purged_volume query\n");
-      free_pool_memory(query);
-      return false;
-   }
-   free_pool_memory(query);
-   Dmsg1(100, "Oldest mediaid=%d\n", oldest.MediaId);
-   if (oldest.MediaId != 0) {
-      mr->MediaId = oldest.MediaId;
-      if (db_get_media_record(jcr, jcr->db, mr)) {
-         if (recycle_volume(jcr, mr)) {
-            Jmsg(jcr, M_INFO, 0, _("Recycled volume \"%s\"\n"), mr->VolumeName);
-            Dmsg1(100, "return 1  recycle_oldest_purged_volume Vol=%s\n", mr->VolumeName);
-            return true;
-         }
+   bstrncpy(mr->VolStatus, "Purged", sizeof(mr->VolStatus));
+   if (db_find_next_volume(jcr, jcr->db, 1, InChanger, mr)) {
+      if (recycle_volume(jcr, mr)) {
+         Jmsg(jcr, M_INFO, 0, _("Recycled volume \"%s\"\n"), mr->VolumeName);
+         Dmsg1(100, "return 1  recycle_oldest_purged_volume Vol=%s\n", mr->VolumeName);
+         return true;
       }
-      Jmsg(jcr, M_ERROR, 0, "%s", db_strerror(jcr->db));
    }
    Dmsg0(100, "return 0  recycle_oldest_purged_volume end\n");
    return false;
