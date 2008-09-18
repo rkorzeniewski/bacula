119450e0551745e69e4c75b09579f4239c27386a
Kern Sibbald <kern@sibbald.com>
2008-09-18 16:02:16 +0000

    Tweak technotes
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@7609 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "2.5.3"
-#define BDATE   "16 September 2008"
-#define LSMDATE "16Sep08"
+#define BDATE   "18 September 2008"
+#define LSMDATE "18Sep08"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2008 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2008"       /* year for copyright messages in progs */
