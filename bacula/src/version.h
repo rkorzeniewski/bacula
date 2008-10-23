b5c5bc739d82c3a9685f1dbffa21dd01ad4752e6
Kern Sibbald <kern@sibbald.com>
2008-10-23 10:12:36 +0000

    kes  Apply patch from Marco van Wieringen that implements the new      Solaris libsec interface for ACLs so that Bacula can save and      restore both the new ACLs and old ACLs. kes  Marco's patch also corrects the file dependency generation code      so that it works properly both with shared libraries and static      libraries. kes  Marco's patch also includes a small cleanup of the cats Makefile      to remove some references to non-existent files.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@7879 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "2.5.17"
-#define BDATE   "21 October 2008"
-#define LSMDATE "21Oct08"
+#define BDATE   "23 October 2008"
+#define LSMDATE "23Oct08"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2008 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2008"       /* year for copyright messages in progs */
