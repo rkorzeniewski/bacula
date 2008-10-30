709f2e8828ec5b3954f4b11a0ede4f4032c851b3
Eric Bollengier <eric@eb.homelinux.org>
2008-10-30 20:45:27 +0000

    ebl  Fix case problem
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@7953 91ce42f0-d328-0410-95d8-f526ca767f89

--- gui/bweb/script/bweb-mysql.sql
+++ gui/bweb/script/bweb-mysql.sql
@@ -41,8 +41,8 @@ UPDATE Status SET severity = 15;
 UPDATE Status SET severity = 100 where JobStatus = 'f';
 UPDATE Status SET severity = 90 where JobStatus = 'A';
 UPDATE Status SET severity = 10 where JobStatus = 'T';
-UPDATE status SET severity = 20 where jobstatus = 'e';
-UPDATE status SET severity = 25 where jobstatus = 'E';
+UPDATE Status SET severity = 20 where jobStatus = 'e';
+UPDATE Status SET severity = 25 where jobStatus = 'E';
 
 
 -- New tables for bresto (same as brestore)
