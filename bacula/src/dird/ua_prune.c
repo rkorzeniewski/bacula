b6e8cdf294b6cb647cd7eb3283dd828911b8b759
Eric Bollengier <eric@eb.homelinux.org>
2008-12-02 18:42:29 +0000

    ebl  Remove extra db_lock() in get_prune_list_for_volume()
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@8103 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/dird/ua_prune.c
+++ bacula/src/dird/ua_prune.c
@@ -472,8 +472,6 @@ int get_prune_list_for_volume(UAContext *ua, MEDIA_DBR *mr, del_ctx *del)
       return 0;                    /* cannot prune Archived volumes */
    }
 
-   db_lock(ua->db);
-
    /*
     * Now add to the  list of JobIds for Jobs written to this Volume
     */
@@ -496,7 +494,6 @@ int get_prune_list_for_volume(UAContext *ua, MEDIA_DBR *mr, del_ctx *del)
    count = exclude_running_jobs_from_list(del);
    
 bail_out:
-   db_unlock(ua->db);
    return count;
 }
 
