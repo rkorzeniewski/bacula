5a6c018f1d8ae4695fb41e015f8606cab6bd4bf4
Kern Sibbald <kern@sibbald.com>
2009-03-24 10:51:48 +0000

    Disable comm timer for now
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@8588 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/qt-console/bcomm/dircomm_auth.cpp
+++ bacula/src/qt-console/bcomm/dircomm_auth.cpp
@@ -1,7 +1,7 @@
 /*
    Bacula® - The Network Backup Solution
 
-   Copyright (C) 2001-2007 Free Software Foundation Europe e.V.
+   Copyright (C) 2001-2009 Free Software Foundation Europe e.V.
 
    The main author of Bacula is Kern Sibbald, with contributions from
    many others, a complete list can be found in the file AUTHORS.
@@ -102,7 +102,8 @@ bool DirComm::authenticate_director(JCR *jcr, DIRRES *director, CONRES *cons,
    }
 
    /* Timeout Hello after 15 secs */
-   dir->start_timer(15);
+// Temp turn this off until we workout how to make sigusr2 work with Qt
+//   dir->start_timer(15);
    dir->fsend(hello, bashed_name);
 
    /* respond to Dir challenge */
