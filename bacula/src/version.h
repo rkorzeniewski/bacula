467e7a34d0a10a87492cd738fdd31e8b2b9630b9
Kern Sibbald <kern@sibbald.com>
2009-04-26 13:02:04 +0000

    Fix platform scripts not to clean configured files during      'make clean' use 'make distclean' to clean everything.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@8747 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "3.0.1"
-#define BDATE   "16 April 2009"
-#define LSMDATE "16Apr09"
+#define BDATE   "26 April 2009"
+#define LSMDATE "26Apr09"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2009 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2009"       /* year for copyright messages in progs */
