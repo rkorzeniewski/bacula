256877d042769328242a262613e226e9b3a7d3e5
Kern Sibbald <kern@sibbald.com>
2009-07-01 18:57:08 +0000

    Update version
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@8936 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "3.0.2"
-#define BDATE   "29 June 2009"
-#define LSMDATE "29Jun09"
+#define BDATE   "01 July 2009"
+#define LSMDATE "01Jul09"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2009 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2009"       /* year for copyright messages in progs */
