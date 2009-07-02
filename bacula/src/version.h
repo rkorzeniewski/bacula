b65f1cf571d45809e8435f4cf87b506327f2106e
Kern Sibbald <kern@sibbald.com>
2009-07-02 09:22:20 +0000

    Another fix for bug #1311 to get the correct last_full_time
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/trunk@8944 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "3.0.2"
-#define BDATE   "01 July 2009"
-#define LSMDATE "01Jul09"
+#define BDATE   "02 July 2009"
+#define LSMDATE "02Jul09"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2009 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2009"       /* year for copyright messages in progs */
