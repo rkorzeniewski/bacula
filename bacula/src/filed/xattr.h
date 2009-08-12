e7943e8edb6f126eb98a524c9c67d7ae35c688a3
Marco van Wieringen <mvw@planets.elm.net>
2009-08-12 22:37:21 +0200

    Move global variables into jcr private xattr data structure so we can have parallel saves of xattr on solaris.

--- bacula/src/filed/xattr.h
+++ bacula/src/filed/xattr.h
@@ -55,6 +55,23 @@ struct xattr_link_cache_entry_t {
 };
 
 /*
+ * Private data structure for threadsafe save of xattrs.
+ * For now its mostly for storing the solaris specific private data.
+ */
+struct xattr_private_data_t {
+   /*
+    * This is the count of xattrs saved on a certain file, it gets reset
+    * on each new file processed and is used to see if we need to send
+    * the hidden xattr dir data. We only send that data when we encounter
+    * an other xattr on the file.
+    */
+   int nr_xattr_saved;
+   char toplevel_hidden_dir_xattr_data[MAXSTRING];
+   int toplevel_hidden_dir_xattr_data_len;
+   alist *xattr_link_cache;
+};
+
+/*
  * Maximum size of the XATTR stream this prevents us from blowing up the filed.
  */
 #define MAX_XATTR_STREAM  (1 * 1024 * 1024) /* 1 Mb */
