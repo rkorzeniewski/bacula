db012fac32af3c79c4843a35709ff0efa88f8730
Eric Bollengier <eric@eb.homelinux.org>
2009-08-26 15:56:37 +0200

    Update JobBytes only for Verify jobs when computing checksum

--- bacula/src/filed/verify.c
+++ bacula/src/filed/verify.c
@@ -361,8 +361,12 @@ static int read_digest(BFILE *bfd, DIGEST *digest, JCR *jcr)
       }
       
       crypto_digest_update(digest, (uint8_t *)buf, n);
-      jcr->JobBytes += n;
-      jcr->ReadBytes += n;
+
+      /* Can be used by BaseJobs, update only for Verify jobs */
+      if (jcr->get_JobLevel() != L_FULL) {
+         jcr->JobBytes += n;
+         jcr->ReadBytes += n;
+      }
    }
    if (n < 0) {
       berrno be;
