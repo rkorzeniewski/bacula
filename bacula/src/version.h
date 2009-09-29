9ccecc84689eace7f861e7680cc208e0727e4af3
Kern Sibbald <kern@sibbald.com>
2009-09-29 20:43:00 +0200

    Update version

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "3.1.4"
-#define BDATE   "26 September 2009"
-#define LSMDATE "26Sep09"
+#define BDATE   "28 September 2009"
+#define LSMDATE "28Sep09"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2009 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2009"       /* year for copyright messages in progs */
