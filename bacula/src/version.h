6024ed258f0a4562d27315f56e33d7730e692a82
Kern Sibbald <kern@sibbald.com>
2009-12-06 10:45:15 +0100

    Update techdoc + notes

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "3.1.6"
-#define BDATE   "01 December 2009"
-#define LSMDATE "01Dec09"
+#define BDATE   "06 December 2009"
+#define LSMDATE "06Dec09"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2009 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2009"       /* year for copyright messages in progs */
