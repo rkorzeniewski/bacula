723f8c6eb4d81a2bfcf3bb0b4ec8f2f642f57405
Kern Sibbald <kern@sibbald.com>
2010-02-13 12:19:43 +0100

    Tweak copyright dates + subroutine name

--- bacula/src/dird/dird_conf.h
+++ bacula/src/dird/dird_conf.h
@@ -1,7 +1,7 @@
 /*
    Bacula® - The Network Backup Solution
 
-   Copyright (C) 2000-2009 Free Software Foundation Europe e.V.
+   Copyright (C) 2000-2010 Free Software Foundation Europe e.V.
 
    The main author of Bacula is Kern Sibbald, with contributions from
    many others, a complete list can be found in the file AUTHORS.
@@ -30,7 +30,6 @@
  *
  *     Kern Sibbald, Feb MM
  *
- *    Version $Id$
  */
 
 /* NOTE:  #includes at the end of this file */
