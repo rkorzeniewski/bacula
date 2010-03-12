11913af4d6259712b7451ec81e4fe45e85aa86b7
Kern Sibbald <kern@sibbald.com>
2010-03-12 08:18:11 +0100

    Tweak: set non-zero level on debug line

--- bacula/src/stored/mount.c
+++ bacula/src/stored/mount.c
@@ -247,7 +247,7 @@ read_volume:
     * Check that volcatinfo is good   
     */
    if (!dev->haveVolCatInfo()) {
-      Dmsg0(000, "Do not have volcatinfo\n");
+      Dmsg0(100, "Do not have volcatinfo\n");
       if (!find_a_volume()) {
          goto mount_next_vol;
       }
