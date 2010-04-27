2a507fbcb595c8491fb6ea67bd0b8ec691faa4fb
Eric Bollengier <eric@eb.homelinux.org>
2010-04-27 21:52:08 +0200

    Update version

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -1,8 +1,8 @@
 
 #undef  VERSION
 #define VERSION "5.0.2"
-#define BDATE   "26 April 2010"
-#define LSMDATE "26Apr10"
+#define BDATE   "28 April 2010"
+#define LSMDATE "28Apr10"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2010 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2010"       /* year for copyright messages in progs */
