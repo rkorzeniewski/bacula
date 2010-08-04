f036687cad79a28e6d8d1cf09284ea1e6988137b
Kern Sibbald <kern@sibbald.com>
2010-08-04 11:57:50 +0200

    Tweak update date

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -1,8 +1,8 @@
 
 #undef  VERSION
 #define VERSION "5.0.3"
-#define BDATE   "02 August 2010"
-#define LSMDATE "02Aug10"
+#define BDATE   "04 August 2010"
+#define LSMDATE "04Aug10"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2010 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2010"       /* year for copyright messages in progs */
