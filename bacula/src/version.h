a4a9b41292e6eb233a1a7bfefe1785f6b536cf09
Kern Sibbald <kern@sibbald.com>
2011-01-26 22:40:53 +0100

    Update version and date

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -1,8 +1,8 @@
 
 #undef  VERSION
-#define VERSION "5.1.8"
-#define BDATE   "24 January 2011"
-#define LSMDATE "24Jan11"
+#define VERSION "5.1.10"
+#define BDATE   "26 January 2011"
+#define LSMDATE "26Jan11"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2010 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2011"       /* year for copyright messages in progs */
