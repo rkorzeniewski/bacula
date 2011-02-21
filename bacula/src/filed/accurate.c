6a54275ece9074344d1a4db7533a3b7d0fc41b65
Kern Sibbald <kern@sibbald.com>
2011-02-21 19:12:33 +0100

    Add more debug to restart regress scripts

--- bacula/src/filed/accurate.c
+++ bacula/src/filed/accurate.c
@@ -1,7 +1,7 @@
 /*
    Bacula® - The Network Backup Solution
 
-   Copyright (C) 2000-2009 Free Software Foundation Europe e.V.
+   Copyright (C) 2000-2011 Free Software Foundation Europe e.V.
 
    The main author of Bacula is Kern Sibbald, with contributions from
    many others, a complete list can be found in the file AUTHORS.
@@ -192,7 +192,7 @@ bool accurate_finish(JCR *jcr)
          }
       }
       accurate_free(jcr);
-      if (!jcr->incomplete && jcr->is_JobLevel(L_FULL)) {
+      if (jcr->is_JobLevel(L_FULL)) {
          Jmsg(jcr, M_INFO, 0, _("Space saved with Base jobs: %lld MB\n"), 
               jcr->base_size/(1024*1024));
       }
