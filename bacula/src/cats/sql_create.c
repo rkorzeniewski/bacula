a0db7c5bf4d53bfc0dab036a82862ab2bfe2f833
Kern Sibbald <kern@sibbald.com>
2012-01-14 10:41:25 +0100

    Fix bug #1810 use single quotes instead of double quotes for MinValue and MaxValue in catalog

--- bacula/src/cats/sql_create.c
+++ bacula/src/cats/sql_create.c
@@ -1,7 +1,7 @@
 /*
    Bacula® - The Network Backup Solution
 
-   Copyright (C) 2000-2010 Free Software Foundation Europe e.V.
+   Copyright (C) 2000-2012 Free Software Foundation Europe e.V.
 
    The main author of Bacula is Kern Sibbald, with contributions from
    many others, a complete list can be found in the file AUTHORS.
@@ -656,7 +656,7 @@ int db_create_counter_record(JCR *jcr, B_DB *mdb, COUNTER_DBR *cr)
    }
    mdb->db_escape_string(jcr, esc, cr->Counter, strlen(cr->Counter));
    /* Must create it */
-   Mmsg(mdb->cmd, "INSERT INTO Counters (Counter,\"MinValue\",\"MaxValue\",CurrentValue,"
+   Mmsg(mdb->cmd, "INSERT INTO Counters (Counter,'MinValue','MaxValue',CurrentValue,"
       "WrapCounter) VALUES ('%s','%d','%d','%d','%s')",
         esc, cr->MinValue, cr->MaxValue, cr->CurrentValue,
         cr->WrapCounter);
