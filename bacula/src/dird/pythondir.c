b68a8a1152275455fe1438aa38612ca7c5a5a8de
Eric Bollengier <eric@baculasystems.com>
2012-01-17 09:24:41 +0100

    Fix python module compilation

--- bacula/src/dird/pythondir.c
+++ bacula/src/dird/pythondir.c
@@ -1,7 +1,7 @@
 /*
    Bacula® - The Network Backup Solution
 
-   Copyright (C) 2004-2008 Free Software Foundation Europe e.V.
+   Copyright (C) 2004-2012 Free Software Foundation Europe e.V.
 
    The main author of Bacula is Kern Sibbald, with contributions from
    many others, a complete list can be found in the file AUTHORS.
@@ -31,7 +31,6 @@
  *
  * Kern Sibbald, November MMIV
  *
- *   Version $Id$
  *
  */
 
@@ -420,7 +419,7 @@ static PyObject *job_cancel(PyObject *self, PyObject *args)
 // PyEval_ReleaseLock();
    UAContext *ua = new_ua_context(jcr);
    ua->batch = true;
-   if (!cancel_job(ua, jcr)) {
+   if (!cancel_job(ua, jcr, true)) {
       /* ***FIXME*** raise exception */
       return NULL;
    }
