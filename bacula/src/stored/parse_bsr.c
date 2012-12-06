e0582cd7da6d78585ebda508c197d5559e91f9d9
Kern Sibbald <kern@sibbald.com>
2012-12-06 10:20:57 +0100

    Tweak update copyright date

--- bacula/src/stored/parse_bsr.c
+++ bacula/src/stored/parse_bsr.c
@@ -1,7 +1,7 @@
 /*
    Bacula® - The Network Backup Solution
 
-   Copyright (C) 2002-2009 Free Software Foundation Europe e.V.
+   Copyright (C) 2002-2012 Free Software Foundation Europe e.V.
 
    The main author of Bacula is Kern Sibbald, with contributions from
    many others, a complete list can be found in the file AUTHORS.
@@ -30,7 +30,6 @@
  *
  *     Kern Sibbald, June MMII
  *
- *   Version $Id$
  */
 
 
