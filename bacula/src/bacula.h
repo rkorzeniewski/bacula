87e42d6d1df06b7990c3ce0a88f4df8c62696f00
Simone Caronni <negativo17@gmail.com>
2014-03-30 20:28:35 +0200

    Remove refs to gnome2,wx-widgets, etc

--- bacula/src/bacula.h
+++ bacula/src/bacula.h
@@ -162,20 +162,6 @@ extern "C" {
 #include "baconfig.h"
 #include "lib/lib.h"
 
-/*
- * For wx-console compiles, we undo some Bacula defines.
- *  This prevents conflicts between wx-Widgets and Bacula.
- *  In wx-console files that malloc or free() Bacula structures
- *  config/resources and interface to the Bacula libraries,
- *  you must use bmalloc() and bfree().
- */
-#ifdef HAVE_WXCONSOLE
-#undef New
-#undef _
-#undef free
-#undef malloc
-#endif
-
 #if defined(HAVE_WIN32)
 #include "win32/winapi.h"
 #include "winhost.h"
