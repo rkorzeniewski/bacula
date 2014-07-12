e8552f823abfaf1c26b4b5993d1ade895bc984ea
Kern Sibbald <kern@sibbald.com>
2014-07-12 15:14:53 +0200

    Tweak version + date

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -1,8 +1,8 @@
 
 #undef  VERSION
-#define VERSION "7.0.4"
-#define BDATE   "04 June 2014"
-#define LSMDATE "04Jun14"
+#define VERSION "7.0.5"
+#define BDATE   "12 July 2014"
+#define LSMDATE "12Jul14"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2014 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2014"       /* year for copyright messages in progs */
